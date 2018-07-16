6484307bd64cc153e6b833943292f62453f84f84
clark_wang <clark_wang@aviva-cofco.com.cn>
2018-07-16 17:51:41 +0800

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path,re_path
+from . import views
+
+urlpatterns = [
+    re_path(r'^$',views.blog_title, name='blog_title'),
+    re_path(r'(?P<article_id>\d)/$', views.blog_article, name="blog_detail"),
+]
---
+++ cmdb/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import HttpResponse
+def index(request):
+    return HttpResponse("Hello world!")
