4d6f409f10cfcedcc585155b1f72fa183109fb4b
clarkwang2008 <luckman_2008@163.com>
2018-07-20 17:27:52 +0800

    Merge branch 'master' of https://github.com/clarkwang2008/HelloDjango
    # Conflicts:
    #	account/views.py
    #	db.sqlite3

--- account/forms.py
+++ account/forms.py
@@ -9,10 +9,10 @@ class RegistrationForm(forms.ModelForm):
     password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
 class Meta:
     model = User
-    field = ("username","email")
+    fields = ("username","email")
 
 def clean_password2(self):
     cd = self.cleaned_data
-    if cd['password'] != cd['password2']
+    if cd['password'] != cd['password2']:
         raise forms.ValidationError("Password do not match")
     return cd['password2']
